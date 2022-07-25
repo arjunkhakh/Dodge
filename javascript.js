2781f0f7f18690d98e301767590ab997787988bf
Arjun Singh <arjunkhakh@gmail.com>
2022-07-25 17:52:18 +0100

    adding obstacles and jump mechanics

--- javascript.js
+++ javascript.js
@@ -5,12 +5,37 @@ document.addEventListener('DOMContentLoaded' , () => {
 
     var birdLeft = 25
     var birdBottom = 50
-    var gravity = 2
+    var gravity = 0.5
 
     function gameStart() {
         birdBottom -= gravity
         bird.style.bottom = birdBottom + '%'
         bird.style.left = birdLeft + '%'
     }
-    gameStart()
+
+    let timerId = setInterval(gameStart, 20)
+    
+    // SpaceBar Jump
+    function control(e) {
+        if (e.keyCode === 32) {
+            jump()
+        }
+    }
+    
+    function jump() {
+        if (birdBottom < 81) {
+            birdBottom += 20
+            bird.style.bottom = birdBottom + "%"
+        }
+        console.log(birdBottom)
+    }
+
+    document.addEventListener('keyup', control)
+
+    function generateObstacle() {
+        const obstacle = document.createElement('div')
+        obstacle.classList.add('obstacle')
+        gameDisplay.append(obstacle)
+    }
+    generateObstacle()
 })
